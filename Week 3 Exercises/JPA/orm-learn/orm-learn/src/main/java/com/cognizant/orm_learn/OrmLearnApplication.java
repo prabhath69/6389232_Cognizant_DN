e148730c3da01f1a482a8aee703b79893ba927dd
prabhath69 <prabhub480@gmail.com>
2025-07-06 01:54:53 +0530

    ORM-Learn
    This commit contains pushes the project that demonstrates the implementation of Object Relational Mapping.

---
+++ Week 3 Exercises/JPA/orm-learn/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,37 @@
+package com.cognizant.orm_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import java.util.List;
+
+import com.cognizant.orm_learn.service.*;
+import com.cognizant.orm_learn.model.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@SpringBootApplication
+public class OrmLearnApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+	private static CountryService countryService;
+
+	public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        countryService = context.getBean(CountryService.class);
+
+        testGetAllCountries();
+
+		LOGGER.info("Inside main");
+	}
+
+    private static void testGetAllCountries() {
+        LOGGER.info("Start");
+        List<Country> countries = countryService.getAllCountries();
+        LOGGER.debug("countries={}", countries);
+        LOGGER.info("End");
+    }
+
+}
