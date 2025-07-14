a9e2269fe346e0f5f726d42c5ab708d60b54a989
prabhath69 <prabhub480@gmail.com>
2025-07-14 10:46:36 +0530

    Week 5

---
+++ Week 5/loan/src/main/java/com/cognizant/loan/controller/LoadController.java
@@ -0,0 +1,41 @@
+package com.cognizant.loan.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/loans")
+public class LoadController {
+
+    @GetMapping("/{number}")
+    public Loan getLoanDetails(@PathVariable String number) {
+        return new Loan(number, "prabhath", 400000, 3258, 18);
+    }
+
+    static class Loan {
+        private String number;
+        private String type;
+        private double loan;
+        private int emi;
+        private int tenure;
+
+        public Loan(String number, String type, double loan, int emi, int tenure) {
+            this.number = number;
+            this.type = type;
+            this.loan = loan;
+            this.emi = emi;
+            this.tenure = tenure;
+        }
+
+        public String getNumber() { return number; }
+        public String getType() { return type; }
+        public double getLoan() { return loan; }
+        public int getEmi() { return emi; }
+        public int getTenure() { return tenure; }
+
+        public void setNumber(String number) { this.number = number; }
+        public void setType(String type) { this.type = type; }
+        public void setLoan(double loan) { this.loan = loan; }
+        public void setEmi(int emi) { this.emi = emi; }
+        public void setTenure(int tenure) { this.tenure = tenure; }
+    }
+}
